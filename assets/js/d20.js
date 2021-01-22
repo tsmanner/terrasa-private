0f58db85b27b44ad36a26b23ae8e835b376c3cc4
Tom Manner <zephrincochrane@gmail.com>
2021-01-22 15:53:27 -0500

    Inserted some debug console output to check something

--- assets/js/d20.js
+++ assets/js/d20.js
@@ -4,6 +4,7 @@ function roll(max, bonus) {
 
 
 function roll_initiative(element) {
+    console.log(document.getElementById(element.id + ": initiative"));
     return document.getElementById(element.id + ": initiative").textContent = roll(20, element.bonus);
 }
 
