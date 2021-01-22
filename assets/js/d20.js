c7c78b733aebd281fe4e2282779c45fe76486b62
Tom Manner <zephrincochrane@gmail.com>
2021-01-22 17:54:12 -0500

    Changed bonus accessor to use getAttribute("bonus")

--- assets/js/d20.js
+++ assets/js/d20.js
@@ -6,7 +6,7 @@ function roll(max, bonus) {
 
 function roll_initiative(element) {
     console.log(element.id + ".initiative = " + document.getElementById(element.id + ".initiative"));
-    return document.getElementById(element.id + ".initiative").textContent = roll(20, element.bonus);
+    return document.getElementById(element.id + ".initiative").textContent = roll(20, element.getAttribute("bonus"));
 }
 
 function map_entities(f) {
