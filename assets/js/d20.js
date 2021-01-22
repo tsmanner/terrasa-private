f00961f830f2fb1cc674264e8a8d2a79ae12d2a8
Tom Manner <zephrincochrane@gmail.com>
2021-01-22 15:55:58 -0500

    Added some more debug

--- assets/js/d20.js
+++ assets/js/d20.js
@@ -4,13 +4,14 @@ function roll(max, bonus) {
 
 
 function roll_initiative(element) {
-    console.log(document.getElementById(element.id + ": initiative"));
+    console.log(element.id + ": initiative = " + document.getElementById(element.id + ": initiative"));
     return document.getElementById(element.id + ": initiative").textContent = roll(20, element.bonus);
 }
 
 function map_entities(f) {
     var entities = document.getElementsByClassName("entity");
     for (let index = 0; index < entities.length; index++) {
+        console.log(entity);
         const entity = entities[index];
         f(entity);
     }
