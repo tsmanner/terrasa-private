a9d6969e5445675b418cc9e02c04c2948ca53ddb
Tom Manner <zephrincochrane@gmail.com>
2021-01-22 17:16:02 -0500

    Added space substitution for entity ids.

--- assets/js/d20.js
+++ assets/js/d20.js
@@ -4,8 +4,8 @@ function roll(max, bonus) {
 
 
 function roll_initiative(element) {
-    console.log(element.id + ": initiative = " + document.getElementById(element.id + ": initiative"));
-    return document.getElementById(element.id + ": initiative").textContent = roll(20, element.bonus);
+    console.log(element.id + ".initiative = " + document.getElementById(element.id + ".initiative"));
+    return document.getElementById(element.id + ".initiative").textContent = roll(20, element.bonus);
 }
 
 function map_entities(f) {
